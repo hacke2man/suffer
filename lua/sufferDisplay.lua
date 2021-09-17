56c254031ab13221db610e9e539807bd937b3135
Liam Paul <liam@pop-stack.org>
2021-09-16 23:50:37 -0700

    show bufer names

--- lua/sufferDisplay.lua
+++ lua/sufferDisplay.lua
@@ -1,4 +1,14 @@
 return function()
-  vim.g.SufferList = table.concat(vim.api.nvim_list_bufs(), ',')
-  return vim.g.SufferList
+  local bufferTable = vim.api.nvim_list_bufs()
+  local SufferList = {}
+
+  for i, bufnum in ipairs(bufferTable) do
+    if vim.api.nvim_buf_is_loaded(bufnum) and vim.api.nvim_buf_is_loaded(bufnum) then
+      if vim.api.nvim_buf_get_name(bufnum) or vim.api.nvim_buf_get_name(bufnum) == '' then
+        table.insert(SufferList, vim.api.nvim_buf_get_name(bufnum))
+      end
+    end
+  end
+
+  return table.concat(SufferList, ' ')
 end
