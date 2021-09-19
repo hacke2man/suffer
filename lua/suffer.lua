b34138ba31e54ae24950320caafbaeee3fafab78
Liam Paul <liam@pop-stack.org>
2021-09-19 14:02:38 -0700

    put everything in one file, its like 40 lines

--- lua/suffer.lua
+++ lua/suffer.lua
@@ -1,5 +1,45 @@
 -- TODO: clean up code a bit
 
+function Suffer_run()
+  local bufferTable = vim.api.nvim_list_bufs()
+  local SufferList = {}
+
+  for i, bufnum in ipairs(bufferTable) do
+    if vim.fn.buflisted(bufnum) == 1 then
+      table.insert(SufferList, vim.api.nvim_exec("echo SufferDisplay(".. bufnum .. ")", true))
+    end
+  end
+
+  local line = table.concat(SufferList, vim.g.SufferSeperator)
+
+  return vim.g.SufferStart .. line .. vim.g.SufferEnd
+end
+
+local Suffer_defaults = {
+  display = function(bufnum) return vim.api.nvim_buf_get_name(bufnum) end,
+  seperator = ' ',
+  line_start = ' ',
+  line_end = ' '
+}
+
 local module = {}
-module.setup = require'sufferSetup'
+module.setup = function(setupInput)
+  local setupDict = Suffer_defaults
+
+  if setupInput then
+    if setupInput.display then setupDict.display = setupInput.display end
+    if setupInput.seperator then setupDict.seperator = setupInput.seperator end
+    if setupInput.line_start then setupDict.line_start = setupInput.line_start end
+    if setupInput.line_end then setupDict.line_end = setupInput.line_end end
+  end
+
+  vim.g.SufferDisplay = setupDict.display
+  vim.g.SufferFilter = setupDict.filter
+  vim.g.SufferSeperator = setupDict.seperator
+  vim.g.SufferStart = setupDict.line_start
+  vim.g.SufferEnd = setupDict.line_end
+  vim.g.SufferRun = Suffer_run
+  vim.o.tabline = "%!SufferRun()"
+end
+
 return module
--- lua/sufferDefaults.lua
+++
@@ -1,6 +0,0 @@
-return {
-  display = function(bufnum) return vim.api.nvim_buf_get_name(bufnum) end,
-  seperator = ' ',
-  line_start = ' ',
-  line_end = ' '
-}
