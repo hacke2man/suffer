d9e59fc8adc55002e15bf682494db231bdbc0af1
Liam Paul <liam@pop-stack.org>
2021-09-19 14:08:19 -0700

    set a consistent naming convention

--- lua/suffer.lua
+++ lua/suffer.lua
@@ -6,13 +6,13 @@ function Suffer_run()
 
   for i, bufnum in ipairs(bufferTable) do
     if vim.fn.buflisted(bufnum) == 1 then
-      table.insert(SufferList, vim.api.nvim_exec("echo SufferDisplay(".. bufnum .. ")", true))
+      table.insert(SufferList, vim.api.nvim_exec("echo suffer_display(".. bufnum .. ")", true))
     end
   end
 
   local line = table.concat(SufferList, vim.g.SufferSeperator)
 
-  return vim.g.SufferStart .. line .. vim.g.SufferEnd
+  return vim.g.suffer_line_start .. line .. vim.g.suffer_line_end
 end
 
 local Suffer_defaults = {
@@ -23,23 +23,23 @@ local Suffer_defaults = {
 }
 
 local module = {}
-module.setup = function(setupInput)
-  local setupDict = Suffer_defaults
+module.setup = function(setup_input)
+  local setup_table = Suffer_defaults
 
-  if setupInput then
-    if setupInput.display then setupDict.display = setupInput.display end
-    if setupInput.seperator then setupDict.seperator = setupInput.seperator end
-    if setupInput.line_start then setupDict.line_start = setupInput.line_start end
-    if setupInput.line_end then setupDict.line_end = setupInput.line_end end
+  if setup_input then
+    if setup_input.display then setup_table.display = setup_input.display end
+    if setup_input.seperator then setup_table.seperator = setup_input.seperator end
+    if setup_input.line_start then setup_table.line_start = setup_input.line_start end
+    if setup_input.line_end then setup_table.line_end = setup_input.line_end end
   end
 
-  vim.g.SufferDisplay = setupDict.display
-  vim.g.SufferFilter = setupDict.filter
-  vim.g.SufferSeperator = setupDict.seperator
-  vim.g.SufferStart = setupDict.line_start
-  vim.g.SufferEnd = setupDict.line_end
-  vim.g.SufferRun = Suffer_run
-  vim.o.tabline = "%!SufferRun()"
+  vim.g.suffer_display = setup_table.display
+  vim.g.suffer_filter = setup_table.filter
+  vim.g.suffer_seperator = setup_table.seperator
+  vim.g.suffer_line_start = setup_table.line_start
+  vim.g.suffer_line_end = setup_table.line_end
+  vim.g.suffer_run = Suffer_run
+  vim.o.tabline = "%!suffer_run()"
 end
 
 return module
