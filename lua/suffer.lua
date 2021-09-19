35e49f709abc93830c4c0ee96810605996b58627
Liam Paul <liam@pop-stack.org>
2021-09-19 15:11:58 -0700

    small cleanup

--- lua/suffer.lua
+++ lua/suffer.lua
@@ -1,7 +1,7 @@
-local line_start, line_end, separator, display
 local module = {}
+local line_start, line_end, separator, display
 
-function Suffer_run()
+local function suffer_run()
   local bufnum_list = vim.api.nvim_list_bufs()
   local buffer_display_list = {}
 
@@ -17,11 +17,12 @@ function Suffer_run()
 end
 
 module.setup = function(setup_input)
-  display = setup_input.display or function(bufnum) return vim.api.nvim_buf_get_name(bufnum) end
+  display = setup_input.display or
+    function(bufnum) return vim.api.nvim_buf_get_name(bufnum) end
   separator = setup_input.separator or ' '
   line_start = setup_input.line_start or ' '
   line_end = setup_input.line_end or ' '
-  vim.g.suffer_run = Suffer_run
+  vim.g.suffer_run = suffer_run
   vim.o.tabline = "%!suffer_run()"
 end
 
